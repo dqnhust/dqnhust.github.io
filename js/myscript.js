df13e7c65f32ed1af8882008e224a13438b03a21
Duong Quang Nam <quangnam.a10@gmail.com>
2017-02-27 12:17:13 +0700

    Add content feature test

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,14 @@
 $.getJSON( "./data.json", function( data ) {
   for (var i = data.length - 1; i >= 0; i--) {
-    alert(data[i].name);
+    var txtAppend = getHTMLContent(data[i].name, data[i].videoURL, data[i].platform, data[i].description, data[i].buttons);
+    $("#content").append(txtAppend);
   };
 });
+
+function getHTMLContent(name, videoURL, platform, description, buttons) {
+  var buttonsHTML = "";
+  for (var i = buttons.length - 1; i >= 0; i--) {
+    buttonsHTML += '<a class="btn btn-primary" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>';
+  }
+  return '<div class="row">    <div class="col-md-7">        <video src = "'+videoURL+'"> </video>    </div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
+}
