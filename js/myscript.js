5c3c74bb503ddf35f37f13d65db57fd0ad4d1686
Duong Quang Nam <quangnam.a10@gmail.com>
2017-02-27 12:30:18 +0700

    complete script

--- js/myscript.js
+++ js/myscript.js
@@ -8,7 +8,7 @@ $.getJSON( "./data.json", function( data ) {
 function getHTMLContent(name, videoURL, platform, description, buttons) {
   var buttonsHTML = "";
   for (var i = buttons.length - 1; i >= 0; i--) {
-    buttonsHTML += '<a class="btn btn-primary" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>';
+    buttonsHTML += '<a class="btn btn-primary" target="_blank" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>';
   }
   return '<div class="row">    <div class="col-md-7"> <center style="background-color: #444444;">       <video src = "'+videoURL+'" controls> </video>    </center></div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
 }
