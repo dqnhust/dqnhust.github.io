2cb7e442ee6b93a469b0a90edfead824d40dfc23
Duong Quang Nam <quangnam.a10@gmail.com>
2017-02-28 00:07:50 +0700

    update

--- js/myscript.js
+++ js/myscript.js
@@ -13,6 +13,6 @@ function getHTMLContent(name, videoURL, platform, description, buttons) {
     buttonsHTML += '<a class="btn btn-primary" target="_blank" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>';
   }
   var strReturn = '<div class="row" id="IDRow'+idF+'">    <div class="col-md-7" ><iframe id="IFURL'+idF+'" width="1px" height="1px" src="'+videoURL+'" frameborder="0" allowfullscreen></iframe></div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
-  strReturn += '<script>  $("#IFURL"'+idF+').height( $("#IDRow"+'+idF+').height() ); </script>';
+  strReturn += '<script>  $("#IFURL'+idF+'").height( $("#IDRow+'+idF+'").height() ); </script>';
   return strReturn;
 }
