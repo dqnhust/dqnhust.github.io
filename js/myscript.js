cff70150e7b2f29557ef49d0aad5b257095dab62
Duong Quang Nam <quangnam.a10@gmail.com>
2017-02-27 23:58:13 +0700

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
+var idF=0;
 $.getJSON( "./data.json", function( data ) {
   for (var i = data.length - 1; i >= 0; i--) {
     var txtAppend = getHTMLContent(data[i].name, data[i].videoURL, data[i].platform, data[i].description, data[i].buttons);
@@ -6,9 +7,12 @@ $.getJSON( "./data.json", function( data ) {
 });
 
 function getHTMLContent(name, videoURL, platform, description, buttons) {
+  idF ++;
   var buttonsHTML = "";
   for (var i = buttons.length - 1; i >= 0; i--) {
     buttonsHTML += '<a class="btn btn-primary" target="_blank" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>';
   }
-  return '<div class="row">    <div class="col-md-7"> <center style="background-color: #444444;">       <video style=" width:700px; height:300px;" src = "'+videoURL+'" controls> </video>    </center></div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
+  var strReturn = '<div class="row" id="IDRow'+idF+'">    <div class="col-md-7" ><iframe id="IFURL'+idF" width="1px" height="1px" src="'+buttons[i].videoURL+'" frameborder="0" allowfullscreen></iframe></div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
+  strReturn += '<script>  $("#IFURL").height( $("#IDRow"+'+idF+').height() ); </script>';
+  return strReturn;
 }
