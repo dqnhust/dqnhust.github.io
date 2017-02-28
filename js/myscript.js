ae9362d45a63f9204c86f482ab0b0f5cf953dc5f
Duong Quang Nam <quangnam.a10@gmail.com>
2017-02-28 13:37:35 +0700

    update

--- js/myscript.js
+++ js/myscript.js
@@ -12,7 +12,7 @@ function getHTMLContent(name, videoURL, platform, description, buttons) {
   for (var i = buttons.length - 1; i >= 0; i--) {
     buttonsHTML += '<a class="btn btn-primary" target="_blank" href="'+buttons[i].url+'">'+buttons[i].text+' <span class="glyphicon glyphicon-chevron-right"></span></a>&nbsp;&nbsp;&nbsp;';
   }
-  var strReturn = '<div class="row" id="IDRow'+idF+'">    <div class="col-md-7" ><iframe id="IFURL'+idF+'" width="100%" height="0.5px" src="'+videoURL+'" frameborder="0" allowfullscreen></iframe></div>    <div class="col-md-5">        <h3>'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
+  var strReturn = '<div class="row" id="IDRow'+idF+'">    <div class="col-md-7" ><iframe id="IFURL'+idF+'" width="100%" height="0.5px" src="'+videoURL+'" frameborder="0" allowfullscreen></iframe></div>    <div class="col-md-5">        <h3 style="margin-top: 0px;">'+name+'</h3>        <h4>'+platform+'</h4>        <p>'+description+'</p>      '+buttonsHTML+'   </div></div><hr>';
   strReturn += '<script>  $("#IFURL'+idF+'").height( $("#IFURL'+idF+'").width()*9/16 ); </script>';
   return strReturn;
 }
